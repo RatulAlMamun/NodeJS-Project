335dc0bc4c15f9dd27502ee02347dd4fcb403d8a
Ratul Al Mamun <ratulalmamun23@gmail.com>
2022-01-04 13:10:19 +0600

    Code refactoring for index file

--- index.js
+++ index.js
@@ -7,33 +7,20 @@
  */
 
 // dependencies
-const http = require('http');
-const environment = require('./helpers/environments');
-const { handleReqRes } = require('./helpers/handleReqRes');
-const { sendTwilioSms } = require('./helpers/notifications');
-
-// @TODO: remove later
-sendTwilioSms(
-    '01965088417',
-    "\nTake my hand\nTake my whole life too\nFor I can't help falling in love with you\n\n<3 Love from Ratul.",
-    (err) => {
-        console.log(err);
-    }
-);
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
 // application scaffolding
 const app = {};
 
-// create server
-app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(environment.port, () => {
-        console.log(`listening to port ${environment.port}`);
-    });
+// application initialization function
+app.init = () => {
+    // start the server
+    server.init();
+    
+    // start the workers
+    workers.init();
 };
 
-// Request & Response Handler
-app.handleReqRes = handleReqRes;
-
-// start the server
-app.createServer();
+// application initiate
+app.init();
